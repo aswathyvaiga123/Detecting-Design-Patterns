b7ad8d8d4b700b405596b5cb6d1a9bdc78002c5a
Aswathymohan <49934409+aswathyvaiga123@users.noreply.github.com>
2021-01-03 12:32:45 +0530

    Delete Client.java

--- Results/src/Observer/Client.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Client {
-	public static void main(String args[]) {
-		System.out.println("\n************** elements **************\n");
-	}
-}
